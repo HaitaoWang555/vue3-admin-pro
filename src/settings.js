04630bc476271746b889c79eb4e4ac8a3df20e6e
wanghaitao <15124505701@163.com>
2021-02-19 11:40:13 +0800

    feat: add components TagsView Settings

--- src/settings.js
+++ src/settings.js
@@ -3,6 +3,18 @@ module.exports = {
 
   /**
    * @type {boolean} true | false
+   * @description Whether show the settings right-panel
+   */
+  showSettings: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether need tagsView
+   */
+  tagsView: true,
+
+  /**
+   * @type {boolean} true | false
    * @description Whether fix the header
    */
   fixedHeader: false,
