d97bad7bcedcf349f5a461549e077067ada1105d
“wanghaitao <15124505701@163.com>
2021-03-06 18:24:02 +0800

    mock

--- mock/public.js
+++ mock/public.js
@@ -1,7 +1,6 @@
 const Mock = require('mockjs')
 
 const data = Mock.mock('@integer(999, 9999)')
-const code = Mock.mock('@pick([20000, 0])')
 
 module.exports = [
   {
@@ -21,8 +20,8 @@ module.exports = [
     type: 'post',
     response: () => {
       return {
-        code: code,
-        msg: code ? 'success' : 'fail',
+        code: 20000,
+        msg: 'success',
       }
     },
   },
