852182c6d1d9e7d105aa1a81e5c3e6922eec7f51
wanghaitao <15124505701@163.com>
2021-04-16 22:08:21 +0800

    Element components (#5)
    * style: 404 width
    
    * feat: add chart
    
    * chore: change build config
    
    * feat: add echarts Map
    
    * style: vue router scrollBehavior
    
    * style: height
    
    * fix: visualMap
    
    * feat: add directive
    
    * feat: use components CardChart
    
    * feat: add chart geo click event
    
    * style: geo options
    
    * feat: add components ProDialog

---
+++ src/components/ProDialog/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="ProDialogWrap">
+    <el-dialog
+      v-model="value"
+      :title="title"
+      :width="width"
+      append-to-body
+      :close-on-click-modal="false"
+      custom-class="ProDialog"
+      @close="handleClose"
+    >
+      <template #title><slot name="title"></slot></template>
+      <slot></slot>
+      <template #footer>
+        <slot v-if="$slots && $slots.footer" name="footer"></slot>
+        <span v-else class="dialog-footer">
+          <el-button @click="handleClose">取 消</el-button>
+          <el-button type="primary" @click="handleClose">确 定</el-button>
+        </span>
+      </template>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProDialog',
+  props: {
+    value: {
+      type: Boolean,
+      default: false,
+    },
+    title: {
+      type: String,
+      default: '提示',
+    },
+    width: {
+      type: String,
+      default: '30%',
+    },
+  },
+  setup(props, { emit }) {
+    function handleClose() {
+      emit('update:value', false)
+    }
+
+    return {
+      handleClose,
+    }
+  },
+}
+</script>
---
+++ src/views/components-page/pro-dialog-view.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <ProDialog
+      v-model:value="visible"
+      title="title"
+      @before-close="handleClose"
+    >
+      封装的Dialog
+    </ProDialog>
+    <el-button
+      style="display: block; margin: 20px auto"
+      type="primary"
+      @click="visible = true"
+      >打开Dialog</el-button
+    >
+  </div>
+</template>
+
+<script>
+import ProDialog from '@/components/ProDialog'
+import { ref } from 'vue'
+export default {
+  name: 'ProDialogPage',
+  components: { ProDialog },
+  setup() {
+    const visible = ref(false)
+
+    function handleClose(params) {
+      console.log(params)
+    }
+
+    return {
+      visible,
+      handleClose,
+    }
+  },
+}
+</script>
+
+<style></style>
